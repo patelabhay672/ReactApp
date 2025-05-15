03d7011e97d078fd76ad6a3b06bd75361660be5a
patelabhay672 <patelabhay0302@gmail.com>
2025-05-15 08:12:08 +0530

    done basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,42 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 const App = () => {
+  const [todos, settodos] = useState([
+    { id: 1, title: "kaamkarna hai", iscompleted: false },
+  ]);
 
-  const [ username, setusername ] = useState("Abhay")
-  const clickHandler = () => {
-    alert('Hello, World!');
-  }
-  const clickHandler2 = (name) => {
-    alert(`Hello, World! ${name}`);
-  }
-
-  const profile = [
-    { name: "Abhay", age: 32 },
-    { name: "devi", age: 21 },
-    { name: "Sita", age: 22 }
-  ]
-
-  const updatedprofile = profile.map((profile, index) => {
-    return (
-      <li>
-        <span>Name: {profile.name}</span><br></br>
-        <span>Age: {profile.age}</span>
-      </li>
-    )
-  })
-
-  const [fullname,setfullname]=useState("")
-
-  const changehandler=(e)=>{
-    setfullname(e.target.value)
-  }
-  console.log(fullname)
-
+  const [title, settitle] = useState("");
+  const [completed, setcompleted] = useState(false);
+  const [gender, setgender] = useState("male");
+  const [city,setcity]=useState("delhi")
   return (
     <div>
-      <h2>{5 + 3}</h2>
-      <h1>My React App</h1>
-      <h2>
-        This is a simple React application that uses Vite for development and
-        build. this is demo . so it can be used for testing and learning purposes.
-      </h2>
-      <button onClick={clickHandler}>Click Me</button>
-      <button onClick={() => clickHandler2('John')}>Click Me 2</button>
-      <ol>{updatedprofile}</ol>
-      <h2>{username}</h2>
-      <button onClick={()=>setusername("Aditya")}>change name</button>
+      <h1>
+        Create task
+      </h1>
+      <form action="">
+        <input
+          type="text"
+          value={title}
+          placeholder="title" onChange={(e) => settitle(e.target.value)} />
+        <br></br>
 
-      <input onChange={changehandler}  type="text" placeholder="fullname"/>
+        <input type="checkbox" checked={completed} onChange={(e) => setcompleted(e.target.checked)} />completed
+        <br></br>
+
+        Male<input type="radio" value="male" onChange={e => setgender("male")} checked={gender == "male" && true} />
+        <br></br>
+
+        female<input type="radio" value="female" onChange={e => setgender("female")} checked={gender == "female" && true} />
+      </form>
+      <br /><br />
+      <select onChange={e => setcity(e.target.value)}>
+        <option value="delhi">Delhi</option>
+        <option value="kolkata">kolkata</option>
+        <option value="mumbai">Mumbai</option>
+      </select>
+
+      <button>create todo</button>
     </div>
   );
 }
